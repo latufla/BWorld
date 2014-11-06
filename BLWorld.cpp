32adeafea26cd089b904953855b996418048ffd0
latufla <lafufla.work@gmail.com>
2014-11-06 18:49:08 +0600

    fix damping exception

--- BLWorld.cpp
+++ BLWorld.cpp
@@ -24,49 +24,21 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	};
 	world.attachShape(1, poly);
 
-// 	world.addObject(2, bl::STATIC_OBJECT, { 1.0f, 4.0f }, (float)M_PI / 2.0f);
-// 	world.attachShape(2, poly);
-
-// 	world.setDensity(1, 0.5f);
-// 	world.setRestitution(1, 0.5f);
-	//world.setFriction(1, 0.5f);
-	
 	std::cout << world.objectToString(1) << std::endl << std::endl;
 
 	world.applyLinearImpulse(1, {0.0f, 1.0f});
-	world.setLinearDamping(1, 0.0f);
+	world.setLinearDamping(1, 1.0f);
 
 	auto ctacts = world.checkContacts(1);
 
-	float stepMSec = 1;
-	world.doStep(1000);
+	float stepMSec = 1000.0f;
+	world.doStep(stepMSec);
 	
 	std::cout << world.objectToString(1) << std::endl << std::endl;
-//	std::cout << world.objectToString(2) << std::endl << std::endl;
-
-	world.doStep(1000);
-
-	std::cout << world.objectToString(1) << std::endl << std::endl;
-//	std::cout << world.objectToString(2) << std::endl << std::endl;
 
 	world.doStep(stepMSec);
 
 	std::cout << world.objectToString(1) << std::endl << std::endl;
-//	std::cout << world.objectToString(2) << std::endl << std::endl;
-	
-	world.doStep(stepMSec);
-
-	std::cout << world.objectToString(1) << std::endl << std::endl;
-//	std::cout << world.objectToString(2) << std::endl << std::endl;
-
-	world.doStep(stepMSec);
-	std::cout << world.objectToString(1) << std::endl << std::endl;
-//	std::cout << world.objectToString(2) << std::endl << std::endl;
-
-	world.doStep(stepMSec);
-	std::cout << world.objectToString(1) << std::endl << std::endl;
-//	std::cout << world.objectToString(2) << std::endl << std::endl;
-
 
 	return 0;
 }
